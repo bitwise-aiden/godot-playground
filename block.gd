35a03a193228c55bc4aff3108679d9b89a34f70d
Aiden Storey <git@storey.dev>
2023-12-09 00:41:29 -0500

    Improve tiles

---
+++ block.gd
@@ -0,0 +1,40 @@
+class_name Block extends Node2D
+
+
+# Private constants
+
+const __SPRITES : Array[Texture] = [
+	preload("res://block_0.png"),
+	preload("res://block_1.png"),
+	preload("res://block_2.png"),
+]
+
+
+# Private variables
+
+@onready var __sprite : Sprite2D = $sprite
+
+
+# Lifecycle methods
+
+func _ready() -> void:
+	__sprite.texture = __SPRITES[randi() % __SPRITES.size()]
+	__sprite.visible = false
+
+
+# Public methods
+
+func move_in(from : float) -> void:
+	__sprite.visible = true
+	__sprite.position.y = from
+
+	var tween : Tween = create_tween()
+
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(
+		__sprite,
+		"position:y",
+		0.0,
+		0.5 + randf() * 0.2,
+	)
--- dungeon.gd
+++ dungeon.gd
@@ -4,16 +4,28 @@ extends TileMap
 # Lifecycle methods
 
 func _ready() -> void:
-	var tween : Tween = create_tween()
+	await get_tree().process_frame
+	await get_tree().create_timer(1.0).timeout
 
-	var original : Vector2i = tile_set.tile_size
-	tile_set.tile_size *= 10
+	for child in get_children(true):
+		if child is Block:
+			var layer : int = __block_layer(child)
+			child.move_in(-200.0 if layer > 0 else 200.0)
+			child.z_index = layer + 1
 
-	tween.set_ease(Tween.EASE_OUT)
-	tween.set_trans(Tween.TRANS_CIRC)
-	tween.tween_property(
-		tile_set,
-		"tile_size",
-		original,
-		0.5
-	)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().reload_current_scene()
+
+
+# Private variables
+
+func __block_layer(block : Block) -> int:
+	var coord : Vector2i = local_to_map(block.position)
+
+	for layer : int in get_layers_count():
+		if get_cell_source_id(layer, coord) != -1:
+			return layer
+
+	return -1
